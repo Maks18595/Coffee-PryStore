78212c15ec522279e47f80f4f27959578dccf8e7
Pryivers <mp972686@gmail.com>
2024-09-26 11:46:59 +0300

    Add project files.

---
+++ Coffee-PryStore/Controllers/PersonRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee_PryStore.Controllers
+{
+    public class PersonRegistrationController : Controller
+    {
+        public IActionResult PersonRegistration()
+        {
+            return View();
+        }
+    }
+}
